382223183343aa74470a9ce0ec7d4ebccb2626d2
David Bonelo <bonelodev@gmail.com>
2024-01-27 17:24:16 -0500

    complete base config and express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("hello salitre");
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
+
+if (process.env.PORT === undefined) {
+  throw Error("Could not find PORT in your environment");
+}
+
+const PORT = parseInt(process.env.PORT);
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
