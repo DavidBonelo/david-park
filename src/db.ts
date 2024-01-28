dade04af25291d4060b8ebdec86754815f42f0c7
David Bonelo <bonelodev@gmail.com>
2024-01-28 17:48:12 -0500

    fix db closing instantly

--- src/db.ts
+++ src/db.ts
@@ -11,15 +11,12 @@ const clientOptions: ConnectOptions = {
 };
 
 export async function runDatabase(): Promise<void> {
-  try {
-    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-    await mongoose.connect(uri, clientOptions);
-    await mongoose.connection.db.admin().command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await mongoose.disconnect();
-  }
+  // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+  await mongoose.connect(uri, clientOptions);
+  await mongoose.connection.db.admin().command({ ping: 1 });
+  console.log("Pinged remote database. successfully connected to MongoDB!");
+}
+
+export function closeDatabase(): void {
+  void mongoose.disconnect();
 }
