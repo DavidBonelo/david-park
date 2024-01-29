d7a7ac286aa1d6267a529ccae7f458e5330b973e
DavidBonelo <bonelodev@gmail.com>
2024-01-29 18:22:23 -0500

    add all staff roles model

--- src/models/staff.ts
+++ src/models/staff.ts
@@ -1,5 +1,8 @@
 import { getModelForClass, prop } from "@typegoose/typegoose";
 import { type Base } from "@typegoose/typegoose/lib/defaultClasses";
+import { type Station } from "./station";
+import { type Customer } from "./customer";
+import { type Attraction } from "./attraction";
 
 export interface Staff extends Base {}
 export abstract class Staff {
@@ -13,3 +16,67 @@ export abstract class Staff {
   available!: boolean;
 }
 
+export class AdminStaff extends Staff {
+  openStation(station: Station): void {
+    station.open = true;
+  }
+
+  closeStation(station: Station): void {
+    station.open = false;
+  }
+}
+const AdminModel = getModelForClass(AdminStaff);
+
+export class LogisticStaff extends Staff {
+  sellTicket(customer: Customer, amount: number): void {
+    customer.credits += amount;
+  }
+}
+const LogisticModel = getModelForClass(LogisticStaff);
+
+export class MarketingStaff extends Staff {
+  sendPromotion(customers: Customer[]): void {
+    customers.forEach((customer) => {
+      if (customer.visits > 3) {
+        console.log("Sending email to " + customer.email + " with promotion");
+      }
+    });
+  }
+}
+const MarketingModel = getModelForClass(MarketingStaff);
+
+export class OperatorStaff extends Staff {
+  customerCanRide(customer: Customer, attraction: Attraction): boolean {
+    if (!attraction.available) return false;
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    if (attraction.minHeight && customer.height >= attraction.minHeight) {
+      return true;
+    }
+    if (attraction.price >= customer.credits) {
+      return true;
+    }
+    return false;
+  }
+}
+const OperatorModel = getModelForClass(OperatorStaff);
+
+export class ManteinanceStaff extends Staff {
+  inspectAttraction(attraction: Attraction): void {
+    if (attraction.daysSinceLastMaintenance > 30) {
+      attraction.available = false;
+    }
+  }
+
+  fixAttraction(attraction: Attraction): void {
+    attraction.available = true;
+  }
+}
+const ManteinanceModel = getModelForClass(ManteinanceStaff);
+
+export {
+  AdminModel,
+  LogisticModel,
+  MarketingModel,
+  OperatorModel,
+  ManteinanceModel,
+};
