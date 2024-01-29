a01f593fb8c919a4cfd60c5a7597b17fc6ae6ad4
DavidBonelo <bonelodev@gmail.com>
2024-01-29 15:12:34 -0500

    add api routes generic error middleware

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -6,5 +6,17 @@ const router = express.Router();
 export default (): express.Router => {
   customers(router);
 
+  router.use(
+    (
+      err: any,
+      req: express.Request,
+      res: express.Response,
+      next: express.NextFunction
+    ) => {
+      // console.error(err.stack);
+      console.error(err.message);
+      res.status(400).send(err.message);
+    }
+  );
   return router;
 };
